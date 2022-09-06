b66647b022fe887317b9c1689916fa90c6bfd16e
kobayashi777 <51016511+kobayashi777@users.noreply.github.com>
2022-09-06 22:46:09 +0900

    create-why-jsx

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>hello world</h1>
-      </header>
-    </div>
-  );
+  return <h1>hello world</h1>;
 }
 
 export default App;
