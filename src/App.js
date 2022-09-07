e3b02a14738d4fc32f395f8f2a0fec7471bd05f7
kobayashi777 <51016511+kobayashi777@users.noreply.github.com>
2022-09-07 21:28:26 +0900

    learn props-types

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
-function App() {
-  return <h1>hello world</h1>;
-}
+import PropTypes from "prop-types";
 
+function App() {
+  const profiles = [
+    { name: "taro", age: 10 },
+    { name: "hanako", age: 5 },
+    { name: "NoName" },
+  ];
+  return (
+    <div>
+      {profiles.map((profile, index) => {
+        return <User name={profile.name} age={profile.age} key={index} />;
+      })}
+    </div>
+  );
+}
+const User = (props) => {
+  return (
+    <div>
+      Hi I am {props.name}, and {props.age} years old!
+    </div>
+  );
+};
+User.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number,
+};
 export default App;
