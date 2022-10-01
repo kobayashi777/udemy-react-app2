b5b9ee5a1b59f2078133adeea1527fe262cad359
kobayashi777 <51016511+kobayashi777@users.noreply.github.com>
2022-10-02 02:07:06 +0900

    redux-basic-finish

--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import reducer from "./reducers";
 import "./index.css";
-import App from "./component/App";
+import App from "./reducers/components/App";
 import reportWebVitals from "./reportWebVitals";
-import registerServiceWorker from "./registerServiceWorker";
+// import registerServiceWorker from "./registerServiceWorker";
 
 const store = createStore(reducer);
 
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(
   <Provider store={store}>
     <App />
-  </Provider>,
-  document.getElementById("root")
+  </Provider>
 );
-registerServiceWorker();
+// registerServiceWorker();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
--- src/reducers/components/App.js
+++ src/reducers/components/App.js
@@ -1,28 +1,26 @@
 import { Component } from "react";
+import { connect } from "react-redux";
+import { increment, decrement } from "../../actions";
 
-function App() {
-  return <Counter></Counter>;
-}
-class Counter extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { count: 0 };
-  }
-  handlePlusButton = () => {
-    this.setState({ count: this.state.count + 1 });
-  };
-  handleMinusButton = () => {
-    this.setState({ count: this.state.count - 1 });
-  };
+class App extends Component {
   render() {
     console.log("this.state", this.state);
+    const props = this.props;
+    console.log("props", props);
     return (
       <>
-        <div>count: {this.state.count}</div>
-        <button onClick={this.handlePlusButton}>+1</button>
-        <button onClick={this.handlePlusButton}>-1</button>
+        <div>value: {props.value}</div>
+        <button onClick={props.increment}>+1</button>
+        <button onClick={props.decrement}>-1</button>
       </>
     );
   }
 }
-export default App;
+const mapStateToProps = (state) => ({ value: state.count.value });
+
+const mapDispatchToProps = (dispatch) => ({
+  increment: () => dispatch(increment()),
+  decrement: () => dispatch(decrement()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
