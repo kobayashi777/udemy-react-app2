e078a93b99f76d55b4ed1c9d7dd558df64ee7b60
kobayashi777 <51016511+kobayashi777@users.noreply.github.com>
2022-10-10 16:06:44 +0900

    events-new changeUdemyCode

--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,14 +1,11 @@
-import _ from "lodash";
-import { READ_EVENTS } from "../actions";
+import _ from 'lodash'
+import { READ_EVENTS } from '../actions'
 
-export const events = (events = {}, action) => {
-  console.log("events");
+export default (events = {}, action) => {
   switch (action.type) {
     case READ_EVENTS:
-      return _.mapKeys(action.response.data, "id");
+      return _.mapKeys(action.response.data, 'id')
     default:
-      return events;
+      return events
   }
-};
-
-export default events;
+}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,5 @@
-import { combineReducers } from "redux";
-import events from "./events";
-console.log("index.js");
-export default combineReducers({ events });
+import { combineReducers } from 'redux'
+import { reducer as form } from 'redux-form'
+import events from './events'
+
+export default combineReducers({ events, form })
